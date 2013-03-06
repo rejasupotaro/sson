587231de9fbab2a3f136a8f92cd127719dce35f0
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-03-06 23:19:33 +0900

    Add TypeAdapter

--- src/com/rejasupotaro/sson/Sson.java
+++ src/com/rejasupotaro/sson/Sson.java
@@ -1,7 +1,5 @@
 package com.rejasupotaro.sson;
 
-import java.io.IOException;
-import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 
 
@@ -20,38 +18,7 @@ public class Sson {
     }
 
     private String toSson(Object src, Type typeOfSrc) {
-        Sexpr rootSexpr = null;
-        Field[] fields = src.getClass().getFields();
-        try {
-            Sexpr lastSexpr = null;
-            for (int i = 0; i < fields.length; i++) {
-                Field field = fields[i];
-                field.setAccessible(true);
-                Parser parser = new Parser(field.getName(), field.get(src).toString());
-                Sexpr sexpr = parser.buildTree();
-                if (rootSexpr == null) {
-                    rootSexpr = sexpr.cloneSexpr(sexpr);
-                    rootSexpr.car = sexpr;
-                    rootSexpr.cdr = null;
-                    lastSexpr = sexpr;
-                } else {
-                    if (rootSexpr.cdr == null) {
-                        rootSexpr.cdr = sexpr;
-                        lastSexpr = rootSexpr.cdr;
-                    } else {
-                        lastSexpr.cdr = sexpr;
-                    }
-                }
-            }
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return rootSexpr == null ? null : rootSexpr.toString();
+        return new TypeAdapter().toSexpr(src);
     }
 
     public static void main(String[] args) {
---
+++ src/com/rejasupotaro/sson/TypeAdapter.java
@@ -0,0 +1,61 @@
+package com.rejasupotaro.sson;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TypeAdapter {
+
+    public String toSexpr(Object src) {
+        Field[] fields;
+        Class<?> clazz = src.getClass();
+        fields = getFieldsAndAddTraversal(clazz);
+        return toSexpr(src, fields);
+    }
+
+    private Field[] getFieldsAndAddTraversal(Class<?> clazz) {
+        List<Field> fieldsList = new ArrayList<Field>();
+        for (Class<?> c = clazz; c != Object.class; c = c.getSuperclass()) {
+            for (Field field : c.getDeclaredFields()) {
+                field.setAccessible(true);
+                fieldsList.add(field);
+            }
+        }
+        return fieldsList.toArray(new Field[fieldsList.size()]);
+    }
+
+    private String toSexpr(Object src, Field[] fields) {
+        Sexpr rootSexpr = null;
+        try {
+            Sexpr lastSexpr = null;
+            for (int i = 0; i < fields.length; i++) {
+                Field field = fields[i];
+                field.setAccessible(true);
+                Parser parser = new Parser(field.getName(), field.get(src).toString());
+                Sexpr sexpr = parser.buildTree();
+                if (rootSexpr == null) {
+                    rootSexpr = sexpr.cloneSexpr(sexpr);
+                    rootSexpr.car = sexpr;
+                    rootSexpr.cdr = null;
+                    lastSexpr = sexpr;
+                } else {
+                    if (rootSexpr.cdr == null) {
+                        rootSexpr.cdr = sexpr;
+                        lastSexpr = rootSexpr.cdr;
+                    } else {
+                        lastSexpr.cdr = sexpr;
+                    }
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return rootSexpr == null ? null : rootSexpr.toString();
+    }
+}
