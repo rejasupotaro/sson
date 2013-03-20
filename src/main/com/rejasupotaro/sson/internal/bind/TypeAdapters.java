5e40217c8868fcc2518fbff3776b67131d868393
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-03-20 19:15:45 +0900

    Add STRING_FACTORY in TypeAdapters

--- src/main/com/rejasupotaro/sson/internal/bind/TypeAdapters.java
+++ src/main/com/rejasupotaro/sson/internal/bind/TypeAdapters.java
@@ -2,7 +2,10 @@ package main.com.rejasupotaro.sson.internal.bind;
 
 import java.io.IOException;
 
+import main.com.rejasupotaro.sson.Sson;
 import main.com.rejasupotaro.sson.TypeAdapter;
+import main.com.rejasupotaro.sson.TypeAdapterFactory;
+import main.com.rejasupotaro.sson.reflect.TypeToken;
 import main.com.rejasupotaro.sson.stream.SexprWriter;
 
 public class TypeAdapters {
@@ -12,4 +15,20 @@ public class TypeAdapters {
             out.value(value);
         }
     };
+
+    public static final TypeAdapterFactory STRING_FACTORY = newFactory(String.class, STRING);
+
+    public static <TT> TypeAdapterFactory newFactory(
+            final Class<TT> type, final TypeAdapter<TT> typeAdapter) {
+        return new TypeAdapterFactory() {
+            @SuppressWarnings("unchecked")
+            public <T> TypeAdapter<T> create(Sson sson, TypeToken<T> typeToken) {
+                return typeToken.getRawType() ==
+                        type ? (TypeAdapter<T>) typeAdapter : null;
+            }
+            @Override public String toString() {
+                return "Factory[type=" + type.getName() + ",adapter=" + typeAdapter + "]";
+            }
+        };
+    }
 }
